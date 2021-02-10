62e925749702ba689d028d52b4b25f00b5e8159f
Adam Bavosa <adam.bavosa@gmail.com>
2021-02-09 18:30:47 -0800

    web page

---
+++ app.js
@@ -0,0 +1,29 @@
+const address = document.getElementById('address');
+const submit = document.getElementById('submit');
+const output = document.getElementById('output');
+
+submit.onclick = () => {
+  if (address.value !== '') {
+    const addr = address.value;
+    fetch('https://api.compound.finance/api/v2/governance/comp/account?address=' + addr)
+      .then(response => response.json())
+      .then(({ markets }) => {
+        try {
+          let compAllocated = 0;
+          let compDistributed = 0;
+          markets.forEach(({ comp_allocated, comp_distributed }) => {
+            compAllocated += +comp_allocated;
+            compDistributed += +comp_distributed;
+          });
+
+          const compAccrued = compAllocated - compDistributed;
+          output.innerText = compAccrued + ' COMP accrued';
+
+        } catch (error) {
+          console.error('data', data);
+          console.error('error', error);
+          output.innerText = 'Request error. Enter a valid address and try again.';
+        }
+      });
+  }
+};
